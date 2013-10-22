d104f8d823e37352fd14ad8d6a34aab65c7da115
josh rotenberg <joshrotenberg@gmail.com>
2013-10-22 14:03:51 -0700

    add package.exs

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "exlydtl", description: "Mix task to precompile erlydtl templates",
+                 version: "0.0.1", keywords: ["erlydtl", "mix"], 
+                 maintainers: [[name: "Josh Rotenberg", 
+                                email: "joshrotenberg@gmail.com"]],
+                 repositories: [[github: "joshrotenberg/exlydtl"]])
+    
