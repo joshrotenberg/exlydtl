b8a2b5c568389b9807fbacf9eac7f55f38117d55
josh rotenberg <joshrotenberg@gmail.com>
2013-10-21 20:55:11 -0700

    make sure the namespace is an atom, add more docs

--- lib/exlydtl.ex
+++ lib/exlydtl.ex
@@ -8,25 +8,33 @@ defmodule Mix.Tasks.Erlydtl do
   with a top level namespace. Assuming a 'templates' directory in the top level of your project with the files
   one.dtl and two.dtl, and the following in your mix.exs ...
 
-  def project do
-    [ app: :my_app,
-      version: "0.1.2",
-      elixir: "~> 0.10.3",
-      deps: deps,
-      erlydtl: [template_path: 'templates',
-                template_namespace: "my_app_templates",
-                template_out_dir: "ebin"]]
-  
+    def project do
+      [ app: :my_app,
+        version: "0.1.2",
+        elixir: "~> 0.10.3",
+        deps: deps,
+        erlydtl: [template_path: 'templates',
+                  template_namespace: "my_app_templates",
+                  template_out_dir: "ebin"]]
+ 
+  ... and then run
+    
+     shell$ mix erlydtl
+
   you would end up with ebin/my_app_templates.beam, and each template could then be called in Elixir code like
 
   :my_app_templates.one([some_var: "foo"])
 
+  Defaults:
+   * template_path - a directory named 'templates' in the project's top level
+   * template_namespace - the app name with "_templates" appended
+   * template_out_dir - the ebin directory in the project's top level
   """
 
   def run(_) do
     IO.puts "Compiling erlydtl templates"
     File.mkdir_p!(template_out_dir)
-    compiled = :erlydtl_compiler.compile_dir(template_path, template_namespace, [out_dir: template_out_dir])
+    :erlydtl_compiler.compile_dir(template_path, template_namespace, [out_dir: template_out_dir])
   end
 
   defp template_path do
@@ -34,7 +42,12 @@ defmodule Mix.Tasks.Erlydtl do
   end
 
   defp template_namespace do
-    (Mix.project[:erlydtl][:template_namespace] || atom_to_binary(Mix.project[:app]) <> "_template")
+    ns = (Mix.project[:erlydtl][:template_namespace] || atom_to_binary(Mix.project[:app]) <> "_template")
+    case is_atom(ns) do
+      true -> ns
+      _ -> binary_to_atom(ns)
+    end
+
   end
 
   defp template_out_dir do
--- mix.exs
+++ mix.exs
@@ -16,6 +16,7 @@ defmodule Exlydtl.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [{:erlydtl, github: "evanmiller/erlydtl"}]
+    [{:erlydtl, github: "evanmiller/erlydtl"},
+       { :ex_doc, github: "elixir-lang/ex_doc" }]
   end
 end
